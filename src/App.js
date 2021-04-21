5850c62b743c24aabfd50e068b9662443776dc1e
Gabriel <gabriel@gabrielhorvat.com>
2021-04-21 11:35:03 +0200

    start on app js fetchSmartContract function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import "./App.css";
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  async function requestAccount() {}
+
+  async function fetchGreeting() {
+    // if using metamask window.eth is present
+  }
+
+  async function setGreeting() {}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
