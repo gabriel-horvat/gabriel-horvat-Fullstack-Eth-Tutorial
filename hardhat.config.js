3690cc5f1b8c295f460a89c86f685adcf0cf10b2
Gabriel <gabriel@gabrielhorvat.com>
2022-07-01 22:12:45 +0200

    add token and erc20 token open zeppelin

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,6 @@ require("@nomiclabs/hardhat-waffle");
 const infuraUrl = process.env.REACT_APP_INFURA_URL;
 const PRIVATE_KEY_1 = process.env.REACT_APP_ACCOUNT_KEY;
 
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -24,13 +16,15 @@ module.exports = {
     artifacts: "./src/artifacts",
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
+    hardhat: {},
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [RINKEBY_ACCOUNT_KEY],
     },
-    ropsten: {
-      url: infuraUrl,
-      accounts: [PRIVATE_KEY_1],
-    },
+    // ropsten: {
+    //   url: infuraUrl,
+    //   accounts: [PRIVATE_KEY_1],
+    // },
   },
   solidity: "0.8.3",
 };
